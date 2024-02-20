8d466a7c42505ce4e06693dd95a229f706719201
MdRakibBari <mdrakibbari01@gmail.com>
2024-02-21 02:43:47 +0600

    finally done the whole game ohoooooooo!!!

--- javaScript/connection.js
+++ javaScript/connection.js
@@ -1,16 +1,63 @@
-function continueGame(){
-    // get a random alphabet
+function keyBoardButtonPress(event) {
+    const playerPressed = event.key;
+    console.log("Player pressed this :", playerPressed )
+
+    if(playerPressed === "Escape"){
+        gameToResult()
+    }
+
+    const randomAlphabet = document.getElementById('different-alphabet');
+    const justText = randomAlphabet.innerText;
+    const randomShorter = justText.toLowerCase();
+
+    if (playerPressed === randomShorter) {
+        const getTheNumber = setAsTextToNumber('score-Inc-Id');
+        const addPlus = getTheNumber + 1;
+        setInSpan('score-Inc-Id', addPlus)
+
+        removeColorOfKey(playerPressed);
+        continueGame();
+
+    }
+    else {
+        const getTheNumber = setAsTextToNumber('life-Dec-Id');
+        const minusOne = getTheNumber - 1;
+        setInSpan('life-Dec-Id', minusOne);
+        if (minusOne === 0) {
+            gameToResult()
+        }
+    }
+}
+document.addEventListener('keyup', keyBoardButtonPress)
+
+function continueGame() {
     const getTheAlphabet = getTheAlphabetArray();
 
-    // set the alphabet as a innertext in paragraph
     const alphabetPlace = document.getElementById('different-alphabet');
-    alphabetPlace.innerText = getTheAlphabet;
-    
-    coloringTheKeyBoard(getTheAlphabet);
+    const bigAlphabet = getTheAlphabet.toUpperCase();
+    alphabetPlace.innerText = bigAlphabet;
+
+    coloringTheKeyBoard(getTheAlphabet)
 }
 
 function homeToGame() {
-    const homeId = addHere('home-Id');
-    const gameId = removeHere('game-Id');
-    continueGame()
+    addHere('home-Id');
+    addHere('result-section')
+    removeHere('game-Id');
+
+    setInSpan('score-Inc-Id', 0)
+    setInSpan('life-Dec-Id', 5)
+    continueGame();
+}
+
+function gameToResult() {
+    addHere('game-Id');
+    removeHere('result-section');
+
+    const lastScore = setAsTextToNumber('score-Inc-Id')
+    setInSpan('game-End-Score', lastScore);
+
+    const getTheAlpha = removeBgFromLastKey("different-alphabet");
+    const doShorter = getTheAlpha.toLowerCase();
+    removeColorOfKey(doShorter)
 }
--- javaScript/utility.js
+++ javaScript/utility.js
@@ -3,20 +3,45 @@ function addHere(elementId) {
     firstId.classList.add("hidden");
 }
 
-function removeHere(elementId){
+function removeHere(elementId) {
     const secondId = document.getElementById(elementId);
     secondId.classList.remove('hidden');
 }
 
-function coloringTheKeyBoard(alphabets){
+function coloringTheKeyBoard(alphabets) {
     const highLightTheKey = document.getElementById(alphabets);
     highLightTheKey.classList.add('bg-orange-400');
 }
 
+function removeColorOfKey(elementId) {
+    const removeColor = document.getElementById(elementId);
+    removeColor.classList.remove('bg-orange-400')
+
+}
+
 function getTheAlphabetArray() {
-    const alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
     const alphabetArray = alphabetString.split('');
     const random = Math.floor(Math.random() * 26);
     const randomAlphabet = alphabetArray[random];
     return randomAlphabet
+}
+
+function setAsTextToNumber(elementId) {
+    const changeElement = document.getElementById(elementId);
+    const gotTheText = changeElement.innerText;
+    const change2Number = parseInt(gotTheText);
+    return change2Number;
+}
+
+function setInSpan(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value
+}
+
+
+function removeBgFromLastKey(elementId){
+    const alphabet = document.getElementById(elementId);
+    const text = alphabet.innerText;
+    return text
 }
